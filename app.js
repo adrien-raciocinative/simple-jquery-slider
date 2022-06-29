874d369a83c12f050b67b7ae968eee5fbab498bc
adrien-raciocinative <108388579+adrien-raciocinative@users.noreply.github.com>
2022-06-29 11:44:47 +0530

    Add files via upload
    basic files for slider

---
+++ app.js
@@ -0,0 +1,27 @@
+jQuery(document).ready(function () {
+  $(".next").on("click", function () {
+    var currentImg = $(".active");
+    var nextImg = currentImg.next();
+
+    if (nextImg.length) {
+      currentImg.removeClass("active");
+      nextImg.addClass("active");
+    }
+  });
+
+  $(".prev").on("click", function () {
+    var currentImg = $(".active");
+    var prevImg = currentImg.prev();
+
+    if (prevImg.length) {
+      currentImg.removeClass("active");
+      prevImg.addClass("active", [2000], [myCallback]);
+    }
+  });
+
+  function myCallback() {
+    setTimeout(function () {
+      $("img").addClass("geeks");
+    }, 3000);
+  }
+});
